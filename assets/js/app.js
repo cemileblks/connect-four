980435a76805164153d97e5713c9161c1cebabef
Cemile <cemile.balkas@sunderlandsoftwarecity.com>
2024-07-12 16:03:47 +0100

    added new function

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,6 +5,36 @@ document.addEventListener('DOMContentLoaded', () => {
   let currentPlayer = 1;
   console.log(squares);
 
+  function checkBoard() {
+    for (let y = 0; y < winningArrays.length; y++) {
+      const square1 = squares[winningArrays[y][0]]
+      const square2 = squares[winningArrays[y][1]]
+      const square3 = squares[winningArrays[y][2]]
+      const square4 = squares[winningArrays[y][3]]
+
+      //check those squares to see if they all have the class of player-one
+      if (
+        square1.classList.contains('player-one') &&
+        square2.classList.contains('player-one') &&
+        square3.classList.contains('player-one') &&
+        square4.classList.contains('player-one')
+      )
+      {
+        result.innerHTML = 'Player One Wins!'
+      }
+      //check those squares to see if they all have the class of player-two
+      if (
+        square1.classList.contains('player-two') &&
+        square2.classList.contains('player-two') &&
+        square3.classList.contains('player-two') &&
+        square4.classList.contains('player-two')
+      )
+      {
+        result.innerHTML = 'Player Two Wins!'
+      }
+    }
+  }
+
   for (let i = 0; i < squares.length; i++) {
 
     for (let i = 0; i < squares.length; i++) {
